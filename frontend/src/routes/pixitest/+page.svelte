2787cc08a840308c5f3f3b329e31d27ad51635c3
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-04-12 17:38:48 +0200

    added mock players

--- frontend/src/routes/pixitest/+page.svelte
+++ frontend/src/routes/pixitest/+page.svelte
@@ -1,61 +1,86 @@
 <script lang="ts">
-	import { Application, Assets, Sprite } from 'pixi.js';
-	import { onMount } from 'svelte';
+    import {Application, Assets} from 'pixi.js';
+    import {onMount} from 'svelte';
+    import {DeckCard, Player} from './script'
 
-	let canvas: HTMLCanvasElement;
+    let canvas: HTMLCanvasElement;
+    let players: any[] = $state([]) // data that should be providen by the websocket
 
-	const app = new Application();
+    $effect(() => {
+        // update the game when a change is made
+        for (let player of players) {
+            player.sprite.hidden(!hasPlayed);
+            player.sprite.setCard(player.card);
+        }
+    })
 
-	onMount(async () => {
-		// Create a PixiJS application.
+    let hasPlayed = $state(false);
+    const app = new Application();
 
-		// Intialize the application.
-		await app.init({ background: '#1099bb', resizeTo: window, canvas: canvas });
+    function validate(selected : number) {
+        hasPlayed = true;
+    }
 
-		let textures = [];
-		let sprites = [];
+    onMount(async () => {
+        // put the card asset in the cache
+        await Assets.load({
+            src: `/cards/Squircle.png`,
+            data: {
+                parseAsGraphicsContext: true
+            }
+        });
 
-		for (let i = 0; i < 14; i++) {
-			const texture = await Assets.load({
-				src: `/cards/${i}.png`,
-				data: {
-					parseAsGraphicsContext: true
-				}
-			});
-			textures.push(texture);
-			const card = new Sprite(texture);
-			sprites.push(card);
-			app.stage.addChild(card);
-			// Center the sprite's anchor point.
-			card.anchor.set(0.5);
+        // Intialize the application.
+        await app.init({background: '#1099bb', resizeTo: window, canvas: canvas});
 
-			// Move the sprite to the center of the screen.
-			card.x = app.screen.width / 2 + 50 * (i - 7);
-			card.y = app.screen.height / 2;
+        // simulate a game with ten players
+        {
+            for (let i = 0; i < 10; i++) {
+                let p = new Player("test", 3)
+                players.push(
+                    {name: "test", card: 3, sprite: p}
+                )
+                p.position.x = 50 + 120 * i;
+                p.position.y = app.screen.height / 4;
+                app.stage.addChild(p);
+            }
+        }
+        // simulate a card change
+        setTimeout(() => {
+            players[0].card = 5
+        }, 8000)
+
+        // initialize the deck
+        let deckCards = []
+        for (let i = 0; i < 14; i++) {
+            const card = new DeckCard(i);
+            deckCards.push(card);
+            app.stage.addChild(card);
+
+            card.x = app.screen.width / 2 + 50 * (i - 7);
+            card.y = 3 * app.screen.height / 4;
             card.zIndex = i
 
-			card.on('pointerover', (event) => {
-				card.scale = 1.1;
-                card.zIndex = 10000
-			});
-			card.on('pointerout', (event) => {
-				card.scale = 1;
-                card.zIndex = i
-			});
-			card.eventMode = 'static';
-		}
+            card.on('pointerdown', (event) => {
+                validate(i)
+                for (let dCard of deckCards) {
+                    dCard.unselect()
+                }
+                card.select()
+            });
+        }
 
-		// // Add an animation loop callback to the application's ticker.
-		// app.ticker.add((time) => {
-		// 	/**
-		// 	 * Just for fun, let's rotate mr rabbit a little.
-		// 	 * Time is a Ticker object which holds time related data.
-		// 	 * Here we use deltaTime, which is the time elapsed between the frame callbacks
-		// 	 * to create frame-independent transformation. Keeping the speed consistent.
-		// 	 */
-		// 	bunny.rotation += 0.1 * time.deltaTime;
-		// });
-	});
+        // // Add an animation loop callback to the application's ticker.
+        // app.ticker.add((time) => {
+        // 	/**
+        // 	 * Just for fun, let's rotate mr rabbit a little.
+        // 	 * Time is a Ticker object which holds time related data.
+        // 	 * Here we use deltaTime, which is the time elapsed between the frame callbacks
+        // 	 * to create frame-independent transformation. Keeping the speed consistent.
+        // 	 */
+        // 	bunny.rotation += 0.1 * time.deltaTime;
+        // });
+    });
 </script>
 
 <canvas bind:this={canvas}></canvas>
---
+++ frontend/src/routes/pixitest/script.ts
@@ -0,0 +1,100 @@
+import {BlurFilter, ColorMatrixFilter, Sprite, Text} from 'pixi.js';
+
+export class Card extends Sprite {
+	text: Text;
+
+	public constructor(i: number) {
+		super(Sprite.from(`/cards/Squircle.png`));
+
+		this.text = new Text({
+			text: `${i}`,
+			style: {
+				fontSize: 64,
+				fill: 'gray',
+				fontFamily: 'righteous'
+			}
+		});
+
+		this.text.anchor.set(0.5);
+		this.text.position.set(0.5);
+
+		this.addChild(this.text);
+
+		this.anchor.set(0.5);
+
+		this.scale.set(0.5);
+	}
+
+	setText(text: string): void {
+		this.text.text = text;
+	}
+}
+
+export class DeckCard extends Card {
+	colorMatrix: ColorMatrixFilter;
+	constructor(i: number) {
+		super(i);
+		const ctx = this;
+		ctx.on('pointerover', (event) => {
+			ctx.scale = 1.1;
+			ctx.zIndex = 10000;
+		});
+		ctx.on('pointerout', (event) => {
+			ctx.scale = 1;
+			ctx.zIndex = i;
+		});
+		ctx.eventMode = 'static';
+
+		this.colorMatrix = new ColorMatrixFilter();
+		this.filters = [this.colorMatrix];
+		this.colorMatrix.contrast(0, false);
+	}
+	select(){
+		this.colorMatrix.matrix = [
+			0, 0, 0, 0, 0, // R
+			0, 0.8, 0, 0, 0, // G
+			0, 0, 0, 0, 0, // B
+			0, 0, 0, 1, 0  // A
+		];
+		this.colorMatrix.contrast(0, true);
+	}
+	unselect(){
+		this.colorMatrix.contrast(0, false);
+	}
+}
+
+export class Player extends Sprite {
+	private playerName: string;
+	private blur = new BlurFilter();
+	private card: Card;
+
+	constructor(name: string, card: number) {
+		super();
+
+		this.filters = [this.blur];
+
+		const text = new Text({
+			text: name,
+			style: {
+				fill: 'white',
+				fontFamily: 'righteous'
+			}
+		});
+
+		this.addChild(text);
+		text.anchor.set(0.5);
+		text.position.set(0.5, 60);
+
+		this.card = new Card(card);
+
+		this.addChild(this.card);
+	}
+
+	setCard(card: number) {
+		this.card.setText(`${card}`);
+	}
+
+	hidden(bool: boolean) {
+		this.blur.blur = bool ? 8 : 0;
+	}
+}
