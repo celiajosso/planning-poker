f4c5079fcb310ec1bf55f4df2884550bce1301bb
elouan-lecorgne <elouan.lecorgne@orange.fr>
2025-04-15 15:32:04 +0200

    Add home page and about page (#33)
    * frontend welcome and about pages
    
    * frontend welcome and about pages
    
    * improve frontend + convert to tailwind css
    
    ---------
    
    Co-authored-by: Elouan Lecorgne <elouanlecorgne@MacBook-Air-de-Elouan.local>
    Co-authored-by: Célia JOSSO <70684974+celiajosso@users.noreply.github.com>

---
+++ frontend/src/lib/button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let { onclick = undefined, href = undefined } = $props();
+    let classes = "bg-[#0e9fc0] text-white border-none px-5 py-3 rounded-lg cursor-pointer m-2 hover:bg-[#0d8ba7]";
+</script>
+
+
+{#if href==undefined}
+<button { onclick } class={classes}>
+    <slot/>
+</button>
+{:else}
+<a {href} class={classes}>
+    <slot/>
+</a>
+{/if}
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Button from "$lib/button.svelte";
+</script>
+
+<div class="w-dvw p-2 text-center text-[#333] flex flex-col h-dvh items-center justify-center bg-[#f4f4f9]">
+    <h1 class="text-4xl font-bold">Welcome to Planning Poker</h1>
+    <p class="text-lg my-4">Collaborate and estimate tasks efficiently with your team.</p>
+    <Button href="/pixitest">Start a Session</Button>
+    <a href="/about" class="text-sm">Learn More</a>
+</div>
+
+
