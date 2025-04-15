a1f8ab681a934d79f1596b53f6d8e488f7f1c3e4
elouan-lecorgne <elouan.lecorgne@orange.fr>
2025-04-15 15:32:04 +0200

    Add home page and about page (#33)
    * frontend welcome and about pages
    
    * frontend welcome and about pages
    
    * improve frontend + convert to tailwind css
    
    ---------
    
    Co-authored-by: Elouan Lecorgne <elouanlecorgne@MacBook-Air-de-Elouan.local>
    Co-authored-by: Célia JOSSO <70684974+celiajosso@users.noreply.github.com>

---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+
+	import Button from "$lib/button.svelte";
+
+</script>
+
+<style>
+
+   
+
+
+
+    .team ul {
+        list-style: none;
+        padding: 0;
+    }
+    .team li {
+        font-size: 1.2rem;
+        margin: 0.5rem 0;
+    }
+</style>
+
+<div class="w-dvw p-2 text-[#333] flex flex-col h-dvh items-center justify-center bg-[#f4f4f9]">
+    <h1 class="text-4xl font-bold">About Planning Poker</h1>
+    <p class="my-4 text-justify max-w-lg">
+        Planning Poker is a collaborative tool designed to help agile teams estimate tasks efficiently. 
+        It fosters better communication, encourages team participation, and ensures accurate task estimation.
+    </p>
+    <div>
+        <h2 class="text-xl font-semibold text-center">Our Team</h2>
+        <div class="flex flex-row justify-around gap-4 mt-2 mb-4">
+            <div>Tom CHAUVEL</div>
+            <div>Elouan LECORGNE</div>
+            <div>Célia JOSSO</div>
+        </div>
+    </div>
+    <Button href="/">Back to Home</Button>
+</div>
