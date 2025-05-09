25c85379450777d9eeca4b77ad1a163f191e83f7
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-09 17:16:31 +0200

    serving static files fixed

--- backend/src/main/kotlin/Routing.kt
+++ backend/src/main/kotlin/Routing.kt
@@ -1,12 +1,35 @@
 package com.example
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
+import io.ktor.server.http.content.default
 import io.ktor.server.http.content.staticFiles
+import io.ktor.server.http.content.staticRootFolder
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondFile
+import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import java.io.File
 
 fun Application.configureRouting() {
     routing {
-        staticFiles("/", File("dist"))
+        staticRootFolder = File("dist")
+        staticFiles( "/", File("dist"))
+
+        get("/{page}") {
+            val page = call.parameters["page"]
+            val file = File("dist/$page.html")
+            val file2 = File("dist/$page")
+            if (file.exists()) {
+                call.respondFile(file)
+            }
+            else if (file2.exists()) {
+                call.respondFile(file2)
+            } else {
+                call.respond(HttpStatusCode.NotFound, "Page not found")
+            }
+        }
+
+        staticFiles("/docs", File("docs"))
     }
 }
