780ed55cb41f0d4b5ab7bd8a2b1d6efc6c7bac55
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-12 21:08:25 +0200

    update user + link
    looks like update user is flaky

--- frontend/src/lib/Game.svelte.ts
+++ frontend/src/lib/Game.svelte.ts
@@ -182,7 +182,7 @@ export namespace Game {
   }
 
   export function updatePlayer(user: UserDTO) {
-    Game.gameContainer.players[user.id].setCard(user.card);
+    Game.gameContainer.updatePlayer(user)
   }
 
   export function createStory(event) {
