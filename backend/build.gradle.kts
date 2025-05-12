d535abc066ef392e89cf3368ab525d5d827bc09d
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-12 20:42:42 +0200

    added pitest

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "2.1.20"
     id("io.ktor.plugin") version "3.1.1"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20"
+    id("info.solidsoft.pitest") version "1.15.0"
 }
 
 group = "com.example"
