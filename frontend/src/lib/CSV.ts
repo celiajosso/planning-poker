6e035bba7dbf5a17a7db6a85e7ae6e408dc05b7e
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-12 18:55:19 +0200

    update

--- frontend/src/lib/CSV.ts
+++ frontend/src/lib/CSV.ts
@@ -1,19 +1,6 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-	import { Icon } from '@steeze-ui/svelte-icon';
-	import { ArrowPath, ArrowRightEndOnRectangle } from '@steeze-ui/heroicons';
-	import { game } from './socket.js';
-	
-
-	let canvas: HTMLCanvasElement;
-
-	onMount(async () => {
-		await game.init(canvas);
-		game.join();
-	});
 
 	function exportVotes() {
-		const votes = game.votes;
+		const votes = Game.storage.room.stories.votes;
 
 		if (!votes || votes.length === 0) {
 			alert("Aucun vote à exporter.");
@@ -132,49 +119,5 @@ function parseAndAddStories(csv: string) {
 //     }
 //     alert("Stories importées !");
 // }
-
-
-
-
-
-
-
-
-
-
-</script>
-
-<canvas bind:this={canvas}></canvas>
-
-<div class="absolute left-0 bottom-4 flex justify-center items-center w-full">
-	<div class="bg-white rounded-xl flex px-4 py-2 gap-4 flex flex-col">
-		<input bind:value={game.playerName} type="text" />
-		<div class="flex items-center justify-center">
-			<button onclick={() => game.restart()}>
-				<Icon class="color-gray-900 size-8" src={ArrowPath} theme="solid" />
-			</button>
-			<button onclick={() => game.quit()}>
-				<Icon class="color-gray-900 size-8" src={ArrowRightEndOnRectangle} theme="solid" />
-			</button>
-			<button onclick={exportVotes}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M11 16h2V7h3l-4-5-4 5h3z"></path><path d="M5 22h14c1.103 0 2-.897 2-2v-9c0-1.103-.897-2-2-2h-4v2h4v9H5v-9h4V9H5c-1.103 0-2 .897-2 2v9c0 1.103.897 2 2 2z"></path></svg>
-			</button>
-			<input
-				type="file"
-				accept=".csv"
-				onchange={(e) => {
-					const input = e.target as HTMLInputElement | null;
-					if (input?.files?.[0]) {
-						importCSV(input.files[0]);
-					}
-				}}
-			/>
-
-			
-			
-			
-		</div>
-	</div>
-</div>
 
 
