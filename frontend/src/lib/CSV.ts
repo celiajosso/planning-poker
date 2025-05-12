eef21d3089dac77da66da69f5bba1b5ede2b6296
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-12 23:47:27 +0200

    Import export (#48)
    * pixidemo (#30)
    
    * pixidemo
    
    * cards
    
    * Game mvp (#32)
    
    * frontend welcome and about pages
    
    * frontend welcome and about pages
    
    * improve frontend + convert to tailwind css
    
    * add export vote as csv file and show import
    
    * update
    
    * update +page.svelte to import jira csv file
    
    * update
    
    * link with our buttons
    
    * clean
    
    * test
    
    ---------
    
    Co-authored-by: Elouan Lecorgne <elouanlecorgne@MacBook-Air-de-Elouan.local>
    Co-authored-by: Célia JOSSO <70684974+celiajosso@users.noreply.github.com>

---
+++ frontend/src/lib/CSV.ts
@@ -0,0 +1,79 @@
+import { Game } from "./Game.svelte";
+
+export function importCSV(file: File) {
+	if (!file) return;
+
+	const reader = new FileReader();
+	reader.onload = (event) => {
+		const text = event.target?.result as string;
+		parseAndAddStories(text);
+		console.log("Import CSV 1");
+
+	};
+	reader.readAsText(file);
+	console.log("Import du fichier :", file.name);
+
+}
+
+export function exportToCSV() {
+	const csvRows: string[] = [];
+
+	const headers = ["Summary", "Description", "Estimate"];
+	csvRows.push(headers.join(","));
+
+	for (const issue of Game.storage.room.stories) {
+		const row = [issue.title, issue.description, issue.finalEstimate];
+		csvRows.push(
+			row.map((value) => `"${value.replace(/"/g, '""')}"`).join(","),
+		);
+	}
+
+	const csvContent = csvRows.join("\n");
+
+	const blob = new Blob([csvContent], {
+		type: "text/csv;charset=utf-8;",
+	});
+	const url = URL.createObjectURL(blob);
+	const link = document.createElement("a");
+	link.setAttribute("href", url);
+	link.setAttribute("download", "issues_export.csv");
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+	URL.revokeObjectURL(url);
+}
+
+function parseAndAddStories(csv: string) {
+	console.log("Import CSV 2");
+
+	const lines = csv.trim().split("\n");
+
+	const separator = lines[0].includes("\t") ? "\t" :
+		lines[0].includes(",") ? "," :
+			";";
+
+	const header = lines[0].split(separator).map(h => h.trim().toLowerCase());
+
+	const summaryIndex = header.findIndex(h => h === "summary");
+	const descriptionIndex = header.findIndex(h => h === "description");
+
+	if (summaryIndex === -1) {
+		alert("Summary missing");
+		return;
+	}
+	else if (descriptionIndex === -1) {
+		alert("Description missing");
+		return;
+	}
+
+	for (let i = 1; i < lines.length; i++) {
+		const cols = lines[i].split(separator);
+		const summary = cols[summaryIndex]?.trim();
+		const description = cols[descriptionIndex]?.trim()
+
+		if (summary) {
+			Game.createStory2(summary, description, "");
+		}
+	}
+	alert("Stories importées !");
+}
