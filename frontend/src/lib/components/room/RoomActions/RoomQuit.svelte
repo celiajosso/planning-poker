2a209e36d4d2d50c1c7ce3fbd2f2984b13f67264
celiajosso <celia.josso@gmail.com>
2025-05-17 16:30:27 +0200

    wip

---
+++ frontend/src/lib/components/room/RoomActions/RoomQuit.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Game } from "../../../Game.svelte";
+  import { goto } from "$app/navigation";
+  import { ArrowRightEndOnRectangle } from "@steeze-ui/heroicons";
+
+  import * as Tooltip from "$lib/components/ui/tooltip";
+  import ButtonIcon from "$lib/ButtonIcon.svelte";
+</script>
+
+<Tooltip.Root>
+  <Tooltip.Trigger>
+    <ButtonIcon
+      onclick={() => {
+        Game.quitRoom();
+        goto("/");
+      }}
+      icon={ArrowRightEndOnRectangle}
+      size="size-6"
+      theme="solid"
+    />
+  </Tooltip.Trigger>
+  <Tooltip.Content><p>Exit Room</p></Tooltip.Content>
+</Tooltip.Root>
--- frontend/src/routes/room/+page.svelte
+++ frontend/src/routes/room/+page.svelte
@@ -1,37 +1,19 @@
 <script lang="ts">
-  import QrCode from "svelte-qrcode";
   import { onMount } from "svelte";
   import { goto } from "$app/navigation";
   import { Game } from "$lib/Game.svelte";
   import { WebSocketManager } from "$lib/WebsocketManager";
-  import * as Select from "$lib/components/ui/select/index.js";
-  import { Icon } from "@steeze-ui/svelte-icon";
-  import {
-    ArrowPath,
-    ArrowRightEndOnRectangle,
-    Share,
-    Square2Stack,
-  } from "@steeze-ui/heroicons";
 
-  import * as Tooltip from "$lib/components/ui/tooltip/index.js";
-  import * as Dialog from "$lib/components/ui/dialog/index.js";
-  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-  import { Button } from "$lib/components/ui/button/index.js";
-  import { Input } from "$lib/components/ui/input/index.js";
-  import { Label } from "$lib/components/ui/label/index.js";
-  import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
-  import ButtonIcon from "$lib/ButtonIcon.svelte";
-  import { copyRoomIdClipboard, copyRoomLinkClipboard, scores } from "./script";
-  import Sheet from "./Sheet.svelte";
-
-  let selectedIssue = null;
+  import Sheet from "../../lib/components/room/Sheet.svelte";
+  import RoomActions from "$lib/components/room/RoomActions.svelte";
 
   let isModifyOpen = $state(false);
   let isAddOpen = $state(false);
+  let loaded = $state(false);
+
+  let selectedIssue = null;
 
   let canvas: HTMLCanvasElement;
-
-  let loaded = $state(false);
 
   onMount(async () => {
     if (WebSocketManager.socket === undefined) {
@@ -43,153 +25,14 @@
   });
 </script>
 
-<canvas bind:this={canvas} class="h-dvh w-dvw"></canvas>
+<canvas bind:this={canvas} class="h-dvh w-dvw" />
 {#if loaded}
-  <Sheet {isModifyOpen} {selectedIssue} {isAddOpen}></Sheet>
-
+  <Sheet {isModifyOpen} {selectedIssue} {isAddOpen} />
   <div class="absolute left-0 bottom-4 flex justify-center items-center w-full">
     <div
       class="bg-white rounded-xl flex px-4 py-2 gap-4 flex-col border-2 border-gray-300"
     >
-      <div class="flex items-center gap-4 justify-center text-gray-700 mt-1">
-        <Tooltip.Root>
-          <Tooltip.Trigger>
-            <ButtonIcon
-              onclick={Game.restart}
-              icon={ArrowPath}
-              size="size-6"
-              theme="solid"
-            ></ButtonIcon>
-          </Tooltip.Trigger>
-          <Tooltip.Content>
-            <p>Reset animation</p>
-          </Tooltip.Content>
-        </Tooltip.Root>
-
-        <Tooltip.Root>
-          <Tooltip.Trigger>
-            <ButtonIcon
-              onclick={() => {
-                Game.quitRoom();
-                goto("/");
-              }}
-              icon={ArrowRightEndOnRectangle}
-              size="size-6"
-              theme="solid"
-            ></ButtonIcon>
-          </Tooltip.Trigger>
-          <Tooltip.Content>
-            <p>Exit Room</p>
-          </Tooltip.Content>
-        </Tooltip.Root>
-
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger>
-            <Tooltip.Root>
-              <Tooltip.Trigger>
-                <ButtonIcon icon={Share} size="size-6" theme="outline"></ButtonIcon>
-              </Tooltip.Trigger>
-              <Tooltip.Content>
-                <p>Share Room</p>
-              </Tooltip.Content>
-            </Tooltip.Root>
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content class="w-40">
-            <DropdownMenu.Label>Share Room</DropdownMenu.Label>
-            <DropdownMenu.Separator />
-            <DropdownMenu.Group>
-              <DropdownMenu.Item>
-                <QrCode
-                  value={`${window.location.origin}?roomId=${Game.storage.room.id}`}
-                />
-              </DropdownMenu.Item>
-              <DropdownMenu.Item onclick={copyRoomIdClipboard}>
-                Share Id
-                <DropdownMenu.Shortcut
-                  ><Icon
-                    class="color-gray-800 size-4"
-                    src={Square2Stack}
-                    theme="outline"
-                  /></DropdownMenu.Shortcut
-                >
-              </DropdownMenu.Item>
-              <DropdownMenu.Item onclick={copyRoomLinkClipboard}>
-                Share link
-                <DropdownMenu.Shortcut
-                  ><Icon
-                    class="color-gray-800 size-4"
-                    src={Square2Stack}
-                    theme="outline"
-                  /></DropdownMenu.Shortcut
-                >
-              </DropdownMenu.Item>
-            </DropdownMenu.Group>
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
-      </div>
+      <RoomActions />
     </div>
-    <Dialog.Root bind:open={isModifyOpen}>
-      <Dialog.Content class="sm:max-w-[425px]">
-        <form
-          onsubmit={(e) => {
-            console.log(e);
-            Game.updateStory(selectedIssue!.id, e);
-            isModifyOpen = false;
-          }}
-        >
-          <Dialog.Header>
-            <Dialog.Title>Edit issue</Dialog.Title>
-            <Dialog.Description>
-              Make changes to your issue here. Click save when you're done.
-            </Dialog.Description>
-          </Dialog.Header>
-          <div class="grid gap-4 py-4">
-            <div class="grid grid-cols-4 items-center gap-4">
-              <Label for="title" class="text-right">Title</Label>
-              <Input
-                name="title"
-                id="title"
-                value={selectedIssue!.title}
-                class="col-span-3"
-                required
-              />
-            </div>
-            <div class="grid grid-cols-4 items-center gap-4">
-              <Label for="description" class="text-right">Description</Label>
-              <Input
-                id="description"
-                name="description"
-                value={selectedIssue!.description}
-                class="col-span-3"
-                required
-              />
-            </div>
-            <div class="grid grid-cols-4 items-center gap-4">
-              <Label for="finalEstimate" class="text-right">Score</Label>
-              <Select.Root>
-                <Select.Trigger class="w-[100px]">
-                  <Select.Value placeholder="Score" />
-                </Select.Trigger>
-                <Select.Content>
-                  <Select.Group>
-                    <ScrollArea class="h-20">
-                      {#each scores as score}
-                        <Select.Item value={score.value} label={score.value}
-                          >{score.value}</Select.Item
-                        >
-                      {/each}
-                    </ScrollArea>
-                  </Select.Group>
-                </Select.Content>
-                <Select.Input name="score" />
-              </Select.Root>
-            </div>
-          </div>
-          <Dialog.Footer>
-            <Button type="submit">Save changes</Button>
-          </Dialog.Footer>
-        </form>
-      </Dialog.Content>
-    </Dialog.Root>
   </div>
 {/if}
