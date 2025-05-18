9e77e630e271f0467861b64da10812ad0059e693
celiajosso <celia.josso@gmail.com>
2025-05-18 11:11:04 +0200

    wip

--- frontend/src/lib/components/room/RoomActions/RoomShare.svelte
+++ frontend/src/lib/components/room/RoomActions/RoomShare.svelte
@@ -1,59 +1,21 @@
 <script>
-  import { Game } from "../../../Game.svelte";
-  import { Icon } from "@steeze-ui/svelte-icon";
-  import { Share, Square2Stack } from "@steeze-ui/heroicons";
-  import QrCode from "svelte-qrcode";
-
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
-  import * as Tooltip from "$lib/components/ui/tooltip";
-  import ButtonIcon from "$lib/ButtonIcon.svelte";
 
-  import {
-    copyRoomIdClipboard,
-    copyRoomLinkClipboard,
-  } from "../../../../routes/room/script";
+  import ShareQrCode from "./Share/ShareQrCode.svelte";
+  import ShareId from "./Share/ShareId.svelte";
+  import ShareLink from "./Share/ShareLink.svelte";
+  import ShareTrigger from "./Share/ShareTrigger.svelte";
 </script>
 
 <DropdownMenu.Root>
-  <DropdownMenu.Trigger>
-    <Tooltip.Root>
-      <Tooltip.Trigger>
-        <ButtonIcon icon={Share} size="size-6" theme="outline"></ButtonIcon>
-      </Tooltip.Trigger>
-      <Tooltip.Content>
-        <p>Share Room</p>
-      </Tooltip.Content>
-    </Tooltip.Root>
-  </DropdownMenu.Trigger>
+  <ShareTrigger />
   <DropdownMenu.Content class="w-40">
     <DropdownMenu.Label>Share Room</DropdownMenu.Label>
     <DropdownMenu.Separator />
     <DropdownMenu.Group>
-      <DropdownMenu.Item>
-        <QrCode
-          value={`${window.location.origin}?roomId=${Game.storage.room.id}`}
-        />
-      </DropdownMenu.Item>
-      <DropdownMenu.Item onclick={copyRoomIdClipboard}>
-        Share Id
-        <DropdownMenu.Shortcut
-          ><Icon
-            class="color-gray-800 size-4"
-            src={Square2Stack}
-            theme="outline"
-          /></DropdownMenu.Shortcut
-        >
-      </DropdownMenu.Item>
-      <DropdownMenu.Item onclick={copyRoomLinkClipboard}>
-        Share link
-        <DropdownMenu.Shortcut
-          ><Icon
-            class="color-gray-800 size-4"
-            src={Square2Stack}
-            theme="outline"
-          /></DropdownMenu.Shortcut
-        >
-      </DropdownMenu.Item>
+      <ShareQrCode />
+      <ShareId />
+      <ShareLink />
     </DropdownMenu.Group>
   </DropdownMenu.Content>
 </DropdownMenu.Root>
---
+++ frontend/src/lib/components/room/RoomActions/Share/ShareQrCode.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Game } from "../../../../Game.svelte";
+  import QrCode from "svelte-qrcode";
+
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+</script>
+
+<DropdownMenu.Item>
+  <QrCode value={`${window.location.origin}?roomId=${Game.storage.room.id}`} />
+</DropdownMenu.Item>
