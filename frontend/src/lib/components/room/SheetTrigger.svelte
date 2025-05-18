638f4dbeceb323a2f8a13c09681d4a7bf8fe2834
celiajosso <celia.josso@gmail.com>
2025-05-18 11:20:39 +0200

    wip

---
+++ frontend/src/lib/components/room/SheetTrigger.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { ChevronDoubleLeft } from "@steeze-ui/heroicons";
+
+  import * as Sheet from "$lib/components/ui/sheet/index.js";
+  import ButtonIcon from "$lib/ButtonIcon.svelte";
+</script>
+
+<Sheet.Trigger class="absolute top-5 right-5 color-gray-700">
+  <ButtonIcon icon={ChevronDoubleLeft} size="size-6" theme="solid"></ButtonIcon>
+</Sheet.Trigger>
