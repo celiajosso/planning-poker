d44891a70f4025777d89c455a640ef2f71e1a44e
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-06-09 23:04:25 +0200

    login ok

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,35 @@
 <script lang="ts">
   import { Toaster } from "svelte-sonner";
   import "../app.css";
-  
+  import { isLogged, username } from "$lib/utils";
+  import { onMount } from "svelte";
+  import randomName from "@scaleway/random-name";
+
   let { children } = $props();
+
+  onMount(() => {
+    if (localStorage.getItem("isLogged")) {
+      isLogged.set(localStorage.getItem("isLogged") === "true");
+      if (localStorage.getItem("username")) {
+        username.set(localStorage.getItem("username")!);
+      }
+    } else {
+      username.set(randomName());
+    }
+
+    isLogged.subscribe((value: boolean) => {
+      if (!value) {
+        localStorage.removeItem("isLogged");
+        username.set(randomName());
+      } else {
+        localStorage.setItem("isLogged", String(value));
+      }
+    });
+
+    username.subscribe((value) => {
+      localStorage.setItem("username", value!);
+    });
+  });
 </script>
 
 {@render children()}
